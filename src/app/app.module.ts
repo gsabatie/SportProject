37f96c968e80a1d5f513c1b585d8eedf8a5382e0
Guillaume <gsabatie@gmail.com>
2017-10-14 19:39:45 +0200

    added firebase conf

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,12 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { environment } from "../environment";
 
 @NgModule({
   declarations: [
@@ -14,7 +18,9 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireDatabaseModule,
   ],
   bootstrap: [IonicApp],
   entryComponents: [
---
+++ src/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    firebaseConfig: {
+        apiKey: "AIzaSyAfU1N_DPjKtjp1xkgrgUu-j6XerbekFBY",
+        authDomain: "sportrproject.firebaseapp.com",
+        databaseURL: "https://sportrproject.firebaseio.com",
+        projectId: "sportrproject",
+        storageBucket: "sportrproject.appspot.com",
+        messagingSenderId: "958207127289"
+    
+    }
+}
